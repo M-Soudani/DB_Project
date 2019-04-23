f2f78d86cb900c4db4006db1eb753ad45a8e1f50
hinaghous <hs.hinashaikh@gmail.com>
2019-04-23 17:52:31 +0200

    Teacher and worker tables added

---
+++ DB Project/src/com/iit/miskolc/model/Teacher.java
@@ -0,0 +1,40 @@
+package com.iit.miskolc.model;
+
+public class Teacher {
+    private int teacherID;
+    private String designation;
+    private int departmentID;
+    private String specialization;
+
+    public int getTeacherID() {
+        return teacherID;
+    }
+
+    public void setTeacherID(int teacherID) {
+        this.teacherID = teacherID;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getDepartmentID() {
+        return departmentID;
+    }
+
+    public void setDepartmentID(int departmentID) {
+        this.departmentID = departmentID;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+}
---
+++ DB Project/src/com/iit/miskolc/model/Worker.java
@@ -0,0 +1,22 @@
+package com.iit.miskolc.model;
+
+public class Worker {
+    private int staffID;
+    private String job_Name;
+
+    public int getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(int staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getJob_Name() {
+        return job_Name;
+    }
+
+    public void setJob_Name(String job_Name) {
+        this.job_Name = job_Name;
+    }
+}
