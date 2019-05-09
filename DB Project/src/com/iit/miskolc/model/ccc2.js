e901aafc66f397fd075bbc7573e6c25ae6b5e363
valeed123 <45469271+valeed123@users.noreply.github.com>
2019-05-09 18:51:26 +0200

    Add files via upload

---
+++ DB Project/src/com/iit/miskolc/model/ccc2.js
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+/**
+ * Person Class
+ *
+ * @author guettatfi
+ *
+ */
+public class coursemodule {
+
+    private int courseID;
+    private String faculty;
+
+    private String institute;
+
+
+
+
+    public int getcourseID() {
+        return courseID;
+    }
+
+    public int setcourseID(int courseID) {
+    this.courseID = courseID;
+}
+
+public String getfaculty() {
+    return faculty;
+}
+
+public void setfaculty(String faculty) {
+    this.faculty = faculty;
+}
+
+public String getinstitute(string institute) {
+    return institute;
+
+    public void setprogramid (String institute){
+        this.institute = institute;
+    }
+
+
+
+
+}
